9b63365546fa7de2ed6e2cc15be994ab9a192a60
PieroLive <73188107@certus.edu.pe>
2021-11-26 12:09:21 -0500

    a
    a

---
+++ resources/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    console.log("Pagina cargada!");
+
+    $('#btnLanding').on('click', function() {
+        $('#SectionOne').hide();
+        $('#SectionTwo').Show();
+    })
+
+    $('#idHome').on('click', function() {
+        $('#SectionOne').hide();
+        $('#SectionTwo').Show();
+    })
+
+});
+
+function actionPage() {
+
+
+}
